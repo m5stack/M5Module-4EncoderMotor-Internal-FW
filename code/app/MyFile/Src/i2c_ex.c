b47189094ea9ed6c1e4fc0bdfb7ebbd85e702386
chenzhjie <jaychen@m5stack.com>
2025-04-14 09:25:15 +0800

    Upgrade to V4: Adjust the soft start function and allow setting individual rotation directions for each motor.

--- code/app/MyFile/Src/i2c_ex.c
+++ code/app/MyFile/Src/i2c_ex.c
@@ -15,7 +15,7 @@
 #define I2C_READ_OPERATION		1
 #define	I2C_RECEIVE_BUFFER_LEN	16
 
-#define FIRMWARE_VERSION 3
+#define FIRMWARE_VERSION 4
 
 static const uint32_t crc_table[0x100] = {
   0x00000000, 0x04C11DB7, 0x09823B6E, 0x0D4326D9, 0x130476DC, 0x17C56B6B, 0x1A864DB2, 0x1E475005, 0x2608EDB8, 0x22C9F00F, 0x2F8AD6D6, 0x2B4BCB61, 0x350C9B64, 0x31CD86D3, 0x3C8EA00A, 0x384FBDBD, 
@@ -143,7 +143,7 @@ __attribute__((weak)) void i2c2_receive_callback(uint8_t *rx_data, uint16_t len)
 		if (rx_data[0] == 0xFF)
 		{
 			if (len == 2) {
-				if (rx_data[1] < 128) {
+				if (rx_data[1] && (rx_data[1] < 128)) {
 					i2c_address[0] = rx_data[1];
 					flash_data_write_back();
 					user_i2c_init();
--- code/app/MyFile/Src/mymotorbdc.c
+++ code/app/MyFile/Src/mymotorbdc.c
@@ -27,13 +27,13 @@
 #define M4F_L GPIOB->BSRR=(uint32_t)(1<<3)<<16
 
 uint32_t normal_motor_pid_velocity_output_ramp = 100;
-float normal_output_rate = 0.0f;
-int16_t setpoint = 0, last_setpoint = 0;
-int16_t output = 0, output_prev = 0;
-uint32_t my_timestamp_now = 0;
-uint32_t my_timestamp_prev = 0;
-float my_Ts = 0.0f;
-uint8_t is_bootup = 0;
+float normal_output_rate[4] = {0};
+int16_t setpoint[4] = {0}, last_setpoint[4] = {0};
+int16_t output[4] = {0}, output_prev[4] = {0};
+uint32_t my_timestamp_now[4] = {0};
+uint32_t my_timestamp_prev[4] = {0};
+float my_Ts[4] = {0};
+uint8_t is_bootup[4] = {0};
 
 void SetMotorDir(uint8_t port,uint8_t dir)
 {
@@ -146,51 +146,51 @@ void SetMotorThrottle(uint8_t port, int8_t throttle)
     int32_t ramp_temp;
 	
     if(config[port][0] == 0x00 && (((soft_start_stop_switch >> port) & 0x01)== 0x01)) {
-        setpoint = throttle;
-        if (last_setpoint != setpoint) {
-            if (abs(last_setpoint) <= 20) {
-                is_bootup = 1;
+        setpoint[port] = throttle;
+        if (last_setpoint[port] != setpoint[port]) {
+            if (abs(last_setpoint[port]) <= 20) {
+                is_bootup[port] = 1;
             }
-            else if (abs(setpoint) <= 20) {
-                is_bootup = 2;
+            else if (abs(setpoint[port]) <= 20) {
+                is_bootup[port] = 2;
             }
-            else if ((setpoint > 0 && last_setpoint < 0) || (setpoint < 0 && last_setpoint > 0)) {
-                is_bootup = 3;
+            else if ((setpoint[port] > 0 && last_setpoint[port] < 0) || (setpoint[port] < 0 && last_setpoint[port] > 0)) {
+                is_bootup[port] = 3;
             }
-            output = setpoint;
-            last_setpoint = setpoint;
+            output[port] = setpoint[port];
+            last_setpoint[port] = setpoint[port];
         }
-        my_timestamp_now = micros();
-        my_Ts = (my_timestamp_now - my_timestamp_prev) * 1e-6f; 
-        if (is_bootup) {
-            if (my_Ts >= 0.1f) {
+        my_timestamp_now[port] = micros();
+        my_Ts[port] = (my_timestamp_now[port] - my_timestamp_prev[port]) * 1e-6f; 
+        if (is_bootup[port]) {
+            if (my_Ts[port] >= 0.1f) {
                 if(normal_motor_pid_velocity_output_ramp > 0) {
                     // limit the acceleration by ramping the output
-                    normal_output_rate = (setpoint - output_prev) / my_Ts;
+                    normal_output_rate[port] = (setpoint[port] - output_prev[port]) / my_Ts[port];
                     ramp_temp = -normal_motor_pid_velocity_output_ramp;
-                    if (normal_output_rate > normal_motor_pid_velocity_output_ramp) {
-                        output = output_prev + normal_motor_pid_velocity_output_ramp * my_Ts;
+                    if (normal_output_rate[port] > normal_motor_pid_velocity_output_ramp) {
+                        output[port] = output_prev[port] + normal_motor_pid_velocity_output_ramp * my_Ts[port];
                     }
-                    else if (normal_output_rate < ramp_temp)
-                        output = output_prev - normal_motor_pid_velocity_output_ramp * my_Ts;
-                    else if ((normal_output_rate <= normal_motor_pid_velocity_output_ramp) && (normal_output_rate >= ramp_temp)) {
-                        output = setpoint;
-                        is_bootup = 0;
+                    else if (normal_output_rate[port] < ramp_temp)
+                        output[port] = output_prev[port] - normal_motor_pid_velocity_output_ramp * my_Ts[port];
+                    else if ((normal_output_rate[port] <= normal_motor_pid_velocity_output_ramp) && (normal_output_rate[port] >= ramp_temp)) {
+                        output[port] = setpoint[port];
+                        is_bootup[port] = 0;
                     }
                 }
 
-                if(output>0)
+                if(output[port]>0)
                 {
                     SetMotorDir(port, MDIR_FFW);
-                    SetMotorPwm(port, output * 255 / 127);
+                    SetMotorPwm(port, output[port] * 255 / 127);
                 }
                 else
                 {
                     SetMotorDir(port, MDIR_REV);
-                    SetMotorPwm(port, (-output) * 255 / 127);   
+                    SetMotorPwm(port, (-output[port]) * 255 / 127);   
                 }        
-                output_prev = output;
-                my_timestamp_prev = my_timestamp_now; 
+                output_prev[port] = output[port];
+                my_timestamp_prev[port] = my_timestamp_now[port]; 
             } 
         }
         else {
@@ -204,8 +204,8 @@ void SetMotorThrottle(uint8_t port, int8_t throttle)
                 SetMotorDir(port, MDIR_REV);
                 SetMotorPwm(port, (-throttle) * 255 / 127);   
             }
-            output_prev = output;
-            my_timestamp_prev = my_timestamp_now;                         
+            output_prev[port] = output[port];
+            my_timestamp_prev[port] = my_timestamp_now[port];                         
         } 
     }
     else {
